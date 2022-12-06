847e009d33c8bf1f469e5a0f7179607539f6e8cf
Vincent Michel <vxgmichel@gmail.com>
2022-12-06 11:43:10 +0100

    Add day06

---
+++ day06/src/main.rs
@@ -0,0 +1,22 @@
+use std::io::{self, BufRead};
+use std::collections::HashSet;
+
+fn solve(stream: &str, size: usize) -> u32 {
+    let stream: Vec<char> = stream.chars().collect();
+    for (i, x) in stream.windows(size).enumerate(){
+        let s: HashSet<char> = x.iter().cloned().collect();
+        if s.len() == size {return i as u32 + size as u32}
+    }
+    panic!()
+}
+
+fn main() {
+    let stream: String = io::stdin()
+        .lock()
+        .lines()
+        .map(|x| x.unwrap())
+        .next().unwrap();
+
+    println!("Part 1: {}", solve(&stream, 4));
+    println!("Part 2: {}", solve(&stream, 14));
+}
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 BASEDIR=$(realpath $(dirname $0))
-for i in {01..05}
+for i in {01..06}
 do
     echo "Day $i"
     echo "------"
